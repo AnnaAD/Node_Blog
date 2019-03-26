e0ce65fdfcdc54337bcc077bf84e956e7f585b40
Anna AD <anna.arpacidusseau@gmail.com>
2019-03-26 08:12:23 -0500

    Inital Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// Setup
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/node-blog",{ useNewUrlParser: true },function(){
+    /* Drop the DB at start-up*/
+    mongoose.connection.db.dropDatabase();
+});
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+var postSchema = new mongoose.Schema({ title: String, body: String, date: String });
+var Post = mongoose.model('Post', postSchema);
+
+// Routes
+app.get("/", (req, res) => {
+	Post.find({}, (err, posts) => {
+		console.log(posts);
+		res.render('index', { posts: posts})
+	    });
+    });
+
+app.post('/addpost', (req, res) => {
+	var postData = new Post(req.body);
+	postData.date = new Date(Date.now()).toLocaleString();
+	postData.save().then( result => {
+		res.redirect('/');
+	}).catch(err => {
+		    res.status(400).send("Unable to save data");
+		});
+		});
+
+// Listen
+app.listen(3000, () => {
+	console.log('Server listing on 3000');
+    });
